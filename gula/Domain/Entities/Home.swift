537dda599c669b1ff43b4777a8d9988f6314752e
Fernando Salom <fer@rudo.es>
2025-09-13 13:16:09 +0200

    Update Home.swift

--- gula/Domain/Entities/Home.swift
+++ gula/Domain/Entities/Home.swift
@@ -15,3 +15,35 @@ struct ActivityItem: Identifiable {
     let subtitle: String
     let time: String
 }
+
+struct GulaStatus {
+    let projectCreated: Date?
+    let gulaVersion: String
+    let installedModules: [GulaModule]
+    let hasProject: Bool
+    let statistics: GulaStatistics?
+    let generatedTemplates: [GulaTemplate]
+}
+
+struct GulaStatistics {
+    let successfulInstalls: Int
+    let generatedTemplates: Int
+    let listingsPerformed: Int
+    let operationsWithError: Int
+    let totalOperations: Int
+}
+
+struct GulaModule: Identifiable {
+    let id = UUID()
+    let name: String
+    let platform: String
+    let branch: String
+    let installDate: Date?
+}
+
+struct GulaTemplate: Identifiable {
+    let id = UUID()
+    let name: String
+    let platform: String
+    let generatedDate: Date?
+}
